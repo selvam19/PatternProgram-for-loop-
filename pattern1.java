d24fc285e9d52ddc69b393730e33998b5928727b
Selvam B <84901905+selvam19@users.noreply.github.com>
2021-06-13 12:27:57 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,23 @@
+class A
+{
+    public static void main(String[] args) {
+        int row=5;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+C:\Users\Admin\.jdks\openjdk-15.0.2\bin\java.exe -javaagent:C:\Users\Admin\Downloads\Compressed\ideaIC-2020.3.2.win\lib\idea_rt.jar=25987:C:\Users\Admin\Downloads\Compressed\ideaIC-2020.3.2.win\bin -Dfile.encoding=UTF-8 -classpath C:\Users\Admin\IdeaProjects\untitled\out\production\untitled A
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+Process finished with exit code 0
